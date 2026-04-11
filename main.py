8b962ef1d5b9a91829b0efb37d616a1386984bdd
Ortega Ascurra Mariano Fabrizio <N00471046@upn.pe>
2026-04-10 22:54:01 -0500

    Probando la multiplicacion

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from resta import ejecutar_resta
+from multiplicacion import ejecutar_multiplicacion
 
 def menu():
     while True:
         print("\n===== MENÚ PRINCIPAL =====")
         print("1. Resta")
-        print("2. Salir")
+        print("2. Multiplicacion")
+        print("3. Salir")
 
         opcion = input("Selecciona una opción: ")
 
         if opcion == "1":
             ejecutar_resta()
         elif opcion == "2":
+            ejecutar_multiplicacion()
+        elif opcion=="3":
             print("Saliendo del programa...")
             break
         else:
             print("Opción inválida, intenta nuevamente.")
-
-
+            
 if __name__ == "__main__":
     menu()
--- multiplicacion.py
+++ multiplicacion.py
@@ -1,8 +1,22 @@
-def multiplicacion (a,b):
-    return a*b
-# ingreso de datos 
-num1= float(input("Ingrese el primer numero: "))
-num2 = float(input("Ingrese el segundo numero: "))
+# Pide un número al que usa el codigo y luego lo valida para que sea correcto
+def pedir_numero(mensaje):
+    return float(input(mensaje))
 
-resultado =multiplicacion (num1,num2)
-print("Resultado: ", resultado)
+def multiplicar(a, b):
+    return a * b
+
+# Función principal de la resta con reintentos
+def ejecutar_multiplicacion():
+    while True:
+        print("\n---MULTIPLICACION ---")
+        
+        num1 = pedir_numero("Ingresa el primer número: ")
+        num2 = pedir_numero("Ingresa el segundo número: ")
+
+        resultado = multiplicar(num1, num2)
+        print(f"Resultado: {resultado}")
+        
+# Opción de repetir
+        opcion = input("¿Deseas hacer otra resta? (si/no): ").lower()
+        if opcion != 'si':
+            break
