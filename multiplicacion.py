858ce00ff055146d4b4f90a00a106fdf906310e9
Ortega Ascurra Mariano Fabrizio <N00471046@upn.pe>
2026-04-10 22:03:30 -0500

    Agregando función de multiplicación

---
+++ multiplicacion.py
@@ -0,0 +1,8 @@
+def multiplicacion (a,b):
+    return a*b
+# ingreso de datos 
+num1= float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
+
+resultado =multiplicacion (num1,num2)
+print("Resultado: ", resultado)
