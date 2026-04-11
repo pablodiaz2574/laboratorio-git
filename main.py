4bc5624b4fbb80e3b52ee92581d842464bda015b
Daniel Jara Alva <n00243181@upn.pe>
2026-04-10 21:12:22 -0500

    Mejorando el menu y la opcion de la resta

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-print("Hola, bienvenido al proyecto")
+from resta import ejecutar_resta
 
-def mensaje_pablo():
-    print("Hola, soy Pablo y estoy aprendiendo Git")
+def menu():
+    while True:
+        print("\n===== MENÚ PRINCIPAL =====")
+        print("1. Resta")
+        print("2. Salir")
+
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            ejecutar_resta()
+        elif opcion == "2":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción inválida, intenta nuevamente.")
+
+
+if __name__ == "__main__":
+    menu()
