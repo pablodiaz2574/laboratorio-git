3c922bb88827a04c75fd8b85e983b8435a5ad9c2
Daniel Jara Alva <n00243181@upn.pe>
2026-04-10 23:33:36 -0500

    Agregando los cambios de menu apra la integracion de la suma

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from resta import ejecutar_resta
 from multiplicacion import ejecutar_multiplicacion
+from suma import ejecutar_suma
 
 def menu():
     while True:
         print("\n===== MENÚ PRINCIPAL =====")
-        print("1. Resta")
-        print("2. Multiplicacion")
-        print("3. Salir")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Multiplicacion")
+        print("4. Salir")
 
         opcion = input("Selecciona una opción: ")
 
         if opcion == "1":
+            ejecutar_suma()
+        if opcion == "2":
             ejecutar_resta()
-        elif opcion == "2":
+        elif opcion == "3":
             ejecutar_multiplicacion()
-        elif opcion=="3":
+        elif opcion=="4":
             print("Saliendo del programa...")
             break
         else:
